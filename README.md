0092336f8ea67998f48ff80872e180737f93888a
Undeadkillz <s.s409_@hotmail.com>
2018-04-27 11:37:31 -0400

    Remove unnecessary devServer code from webpack config Add babel-polyfill to support older browsers (ones that don't have Promise support for example) Update for README file with all relevant information to project.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Frontend - Webapp
+# About
+A social media management panel for [SocialIgnite](https://socialignite.media) built with AngularJS. Allows users to schedule, moderate and monitor their social media pages on Facebook, Twitter, LinkedIn and Instagram.
 
+
+
+## How to contribute:
+
+[![Discord Group](https://socialignite.media/wp-content/uploads/2018/04/Discord-LogoWordmark-Color.png)](https://discord.gg/jnravke)
+
+~~~
+git clone https://github.com/Undeadkillz/social-ignite-webapp.git # Clone project files locally
+node install # To install the dependencies
+~~~
+The project is still in early stages, and any feedback or contribution is appreciated.
+
+#### Formatting:
+Simply make your desired changes and submit your pull request. At this point, there is no specific format you have to follow yet, just make sure the code is easily readable, and if possible comment where possible so that the pull request could be approved faster.
+
+
+
+### [SocialIgnite - Social Media Management](https://socialignite.media)
+[![SocialIgnite](https://socialignite.media/wp-content/uploads/2018/04/source_file.png)](https://socialignite.media)
+
+[![forthebadge](http://forthebadge.com/images/badges/uses-js.svg)](http://forthebadge.com)
+[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)
--- webpack.config.js
+++ webpack.config.js
@@ -6,35 +6,21 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
     context: __dirname + "/src/app",
     entry: {
-        app: ["./bootstrap.js"],
+        app: ['babel-polyfill',"./bootstrap.js"],
     },
     output: {
         path: __dirname + "/dist/pub/",
         filename: "[name].js"
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        public: "portal.socialignite.media",
-        before: function (app){
-            app.get("*", function(req, res, next) {
-                var url = req.url;
-                if (
-                    url.endsWith(".js") ||
-                    url.startsWith("/pub/")) return next();
-                else res.sendFile(path.join(__dirname, "dist/pub/index.html"))
-            });
-        }
     },
 
     plugins: [
         new webpack.DefinePlugin({
             "API": JSON.stringify("https://portal.socialignite.media:8000")
         }),
-        // new UglifyJsPlugin({
-        //     parallel: 4,
-        //     sourceMap: true
-        // })
+        new UglifyJsPlugin({
+            parallel: 4,
+            sourceMap: true
+        })
     ],
 
     resolve: {
@@ -49,9 +35,9 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
-                // use: {
-                //     loader: "babel-loader",
-                // }
+                use: {
+                    loader: "babel-loader",
+                }
             },
             { test: require.resolve("moment"), loader: "expose-loader?moment" },
             {
